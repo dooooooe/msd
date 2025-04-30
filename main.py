2e108cbbe2a8feffb7969641da67bc2aa2e5bfc4
dooooooe <truegodly@gmail.com>
2025-04-30 15:06:39 -0700

    league

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import discord
-from discord.ext import commands
-
 import time
 from humanize import precisedelta
 import random
@@ -9,6 +6,8 @@ import json
 import shutil
 from dotenv import load_dotenv
 
+import discord
+from discord.ext import commands
 bot = commands.Bot(command_prefix=',', intents=discord.Intents.all(), help_command=None)
 
 @bot.event
@@ -17,9 +16,10 @@ async def on_ready():
 
 # game setup
 COUNTDOWN = 20
-DECK = 'limbus'
-THEME = 'Limbus Company'
+DECK = 'league'
+THEME = 'League of Legends'
 COOLDOWN = 600
+CARDS = 10
 
 shutil.copyfile(f'./decks/{DECK}.txt', './decks/deck.txt')
 
@@ -157,6 +157,14 @@ async def on_message_delete(message):
             await message.author.send('A card you played seems to have been deleted...')
 
 
+@bot.event
+async def on_message_edit(message, after):
+    for card in played_cards:
+        if message.id == card.message.id and card.prompt.strip() not in after.content:
+            played_cards.remove(card)
+            await message.author.send('A card you played was edited out of existence...')
+
+
 # bot commands
 @bot.command(name='join')
 async def join(ctx):
@@ -170,16 +178,23 @@ async def join(ctx):
     with open('./userdata/cooldowns.json', 'r') as f:
         cooldowns = json.load(f)
 
+    with open('./userdata/wins.json', 'r') as f:
+        wins = json.load(f)
+
     scoreboard[ctx.author.id] = 0
     cooldowns[ctx.author.id] = 0
-    
+    wins[ctx.author.id] = 0 if ctx.author.id not in wins else wins[ctx.author.id]
+
     with open('./userdata/scoreboard.json', 'w') as f:
         json.dump(scoreboard, f, indent=4)
 
     with open('./userdata/cooldowns.json', 'w') as f:
         json.dump(cooldowns, f, indent=4)
 
-    await give_cards(ctx.author, 3)
+    with open('./userdata/wins.json', 'w') as f:
+        json.dump(wins, f, indent=4)
+
+    await give_cards(ctx.author, CARDS)
 
     await ctx.reply(f'{ctx.author.name} joined!')
     print(f'{ctx.author.name} joined')
@@ -229,6 +244,10 @@ async def cards(ctx):
 
 @bot.command(name='leaderboard', aliases=['lb', 'scoreboard'])
 async def leaderboard(ctx):
+    if ctx.channel.id == 1337502693903831061:
+        await ctx.reply('Please use this command in https://discord.com/channels/1025958887209316422/1313998443827691551 to avoid clutter.')
+        return
+    
     with open('./userdata/scoreboard.json', 'r') as f:
         scoreboard = json.load(f)
 
@@ -241,6 +260,30 @@ async def leaderboard(ctx):
 
     embed = discord.Embed(
         title='LEADERBOARD',
+        description=description
+    )
+
+    await ctx.send(embed=embed)
+
+
+@bot.command(name='wins')
+async def wins(ctx):
+    if ctx.channel.id == 1337502693903831061:
+        await ctx.reply('Please use this command in https://discord.com/channels/1025958887209316422/1313998443827691551 to avoid clutter.')
+        return
+    
+    with open('./userdata/wins.json', 'r') as f:
+        wins = json.load(f)
+
+    sorted_scores = sorted(wins.items(), key=lambda x: x[1], reverse=True)
+
+    description = ''
+    for i, (user_id, score) in enumerate(sorted_scores, 1):
+        user = await bot.fetch_user(int(user_id))
+        description += f'**{i}. {user.name}**{score}\n\n'
+
+    embed = discord.Embed(
+        title='ALL TIME WINS',
         description=description
     )
 
@@ -265,9 +308,10 @@ async def cooldown(ctx):
 async def help(ctx):
     if ctx.channel.id == 1337502693903831061:
         await ctx.reply('Please use this command in https://discord.com/channels/1025958887209316422/1313998443827691551 to avoid clutter.')
-    else:
-        await ctx.reply(embed=discord.Embed(title='DA RULES', 
-                                            description=f'Each player holds 3 cards at a time each with a different prompt and the primary objective of the game is to use these prompts in conversation without getting caught (only works in https://discord.com/channels/1025958887209316422/1337502693903831061).\n\nIf you attempt to slip in a prompt and are not caught for 20 messages, then you will be granted +5 points. However, if you are caught before then, then you will lose -1 point.\n\nIf you suspect someone is trying to slip in one of their prompts, you may report them in order to gain +1 point. If you are wrong, however then you will lose -1 point.\n\nTo avoid spam, you may only slip in a card every {precisedelta(COOLDOWN)}. Additionally using multiple prompts in one message will only count one.\n\nAfter a period of time, a new deck with a different theme will be rotated in; at this time, whoever has the most points will win the game and a new game will be started. Good luck!\n\n**Points**\nSlipping in a card: +5\nGetting caught: -1\nCatching someone: +2\nFalse reporting: -1\n\n**Commands**\n`,join` - Join the game\n`,report` - Reply to a message with this command to report it\n`,cards` - View your cards (in DMs)\n`,leaderboard` - View the current points leaderboard\n\n*Reminder that commands work in bot DMs*\n\nCurrent Theme:\n**{THEME}**'))
+        return
+    
+    await ctx.reply(embed=discord.Embed(title='DA RULES', 
+                                        description=f'Each player holds {CARDS} cards at a time each with a different prompt and the primary objective of the game is to use these prompts in conversation without getting caught (only works in https://discord.com/channels/1025958887209316422/1337502693903831061).\n\nIf you attempt to slip in a prompt and are not caught for 20 messages, then you will be granted +5 points. However, if you are caught before then, then you will lose -1 point.\n\nIf you suspect someone is trying to slip in one of their prompts, you may report them in order to gain +1 point. If you are wrong, however then you will lose -1 point.\n\nTo avoid spam, you may only slip in a card every {precisedelta(COOLDOWN)}. Additionally using multiple prompts in one message will only count one.\n\nAfter a period of time, a new deck with a different theme will be rotated in; at this time, whoever has the most points will win the game and a new game will be started. Good luck!\n\n**Points**\nSlipping in a card: +5\nGetting caught: -1\nCatching someone: +2\nFalse reporting: -1\n\n**Commands**\n`,join` - Join the game\n`,report` - Reply to a message with this command to report it\n`,cards` - View your cards\n`,cooldown` - Check your remaining cooldown\n`,leaderboard` - View the current points leaderboard\n\n*Reminder that commands work in bot DMs*\n\nCurrent Theme:\n**{THEME}**'))
 
 
 # run bot
